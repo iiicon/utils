37f851161ac53179a741bed693a38c4cfc6b4c8e
GerritV <shigw5420@joyowo.com>
2020-06-16 15:38:16 +0800

    curry

---
+++ src/subject/curry.js
@@ -0,0 +1,100 @@
+function curry(fn, args) {
+  var length = fn.length;
+
+  args = args || [];
+
+  return function () {
+    var _args = args.slice(0),
+      arg,
+      i;
+
+    for (i = 0; i < arguments.length; i++) {
+      _args.push(arg[i]);
+    }
+
+    if (_args.length < length) {
+      return curry.call(this, fn, _args);
+    } else {
+      return curry.apply(this, _args);
+    }
+  };
+}
+
+// 第二版
+function sub_curry(fn) {
+  var args = [].slice.call(arguments, 1);
+  return function () {
+    return fn.apply(this, args.concat([].slice.call(arguments)));
+  };
+}
+
+function curry(fn, length) {
+  length = length || fn.length;
+
+  var slice = Array.prototype.slice;
+
+  return function () {
+    if (arguments.length < length) {
+      var combined = [fn].concat(slice.call(arguments));
+      return curry(sub_curry.apply(this, combined), length - arguments.length);
+    } else {
+      return fn.apply(this, arguments);
+    }
+  };
+}
+
+// 包含占位符的版本 第三版
+// TODO: 理解占位符的方案
+function curry(fn, args, holes) {
+  length = fn.length;
+
+  args = args || [];
+
+  holes = holes || [];
+
+  return function () {
+    var _args = args.slice(0),
+      _holes = holes.slice(0),
+      argsLen = args.length,
+      holesLen = holes.length,
+      arg,
+      i,
+      index = 0;
+
+    for (i = 0; i < arguments.length; i++) {
+      arg = arguments[i];
+      // 处理类似 fn(1, _, _, 4)(_, 3) 这种情况，index 需要指向 holes 正确的下标
+      if (arg === _ && holesLen) {
+        index++;
+        if (index > holesLen) {
+          _args.push(arg);
+          _holes.push(argsLen - 1 + index - holesLen);
+        }
+      }
+      // 处理类似 fn(1)(_) 这种情况
+      else if (arg === _) {
+        _args.push(arg);
+        _holes.push(argsLen + i);
+      }
+      // 处理类似 fn(_, 2)(1) 这种情况
+      else if (holesLen) {
+        // fn(_, 2)(_, 3)
+        if (index >= holesLen) {
+          _args.push(arg);
+        }
+        // fn(_, 2)(1) 用参数 1 替换占位符
+        else {
+          _args.splice(_holes[index], 1, arg);
+          _holes.splice(index, 1);
+        }
+      } else {
+        _args.push(arg);
+      }
+    }
+    if (_holes.length || _args.length < length) {
+      return curry.call(this, fn, _args, _holes);
+    } else {
+      return fn.apply(this, _args);
+    }
+  };
+}
